cda4594de2d32dec4adb674808cfee00cc192cad
echo <lonelybutts@gmail.com>
2020-06-13 17:23:40 +0700

    Updates

---
+++ precache-manifest.e4524763fb0b16b495b53db62513348d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d7677001ec1a22d367a74d9ee1ca7a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6d8fcc1189fa1f189b06",
+    "url": "/monster-rolodex/static/css/main.4a692132.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8fcc1189fa1f189b06",
+    "url": "/monster-rolodex/static/js/main.93e16e16.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
